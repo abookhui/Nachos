2de41387853601c0842d1d110b4e825903b803c9
ldh0784 <ldhabc58@gmail.com>
2025-05-20 21:24:45 +0900

    dh 2-2 condition2

--- threads/Condition2.java
+++ threads/Condition2.java
@@ -2,6 +2,8 @@ package nachos.threads;
 
 import nachos.machine.*;
 
+import java.util.LinkedList;
+
 /**
  * An implementation of condition variables that disables interrupt()s for
  * synchronization.
@@ -21,7 +23,7 @@ public class Condition2 {
      *				<tt>wake()</tt>, or <tt>wakeAll()</tt>.
      */
     public Condition2(Lock conditionLock) {
-	this.conditionLock = conditionLock;
+        this.conditionLock = conditionLock;
     }
 
     /**
@@ -31,11 +33,17 @@ public class Condition2 {
      * automatically reacquire the lock before <tt>sleep()</tt> returns.
      */
     public void sleep() {
-	Lib.assertTrue(conditionLock.isHeldByCurrentThread());
+        Lib.assertTrue(conditionLock.isHeldByCurrentThread());
 
-	conditionLock.release();
+        boolean interrupt = Machine.interrupt().disable(); // 인터럽트를 disable시킴
+        waitQueue.add(KThread.currentThread()); // 현재 스레드를 대기 큐에 추가하고
 
-	conditionLock.acquire();
+        conditionLock.release();
+
+        KThread.currentThread().sleep();
+
+        conditionLock.acquire();
+        Machine.interrupt().restore(interrupt); // disable된 인터럽트를 restore
     }
 
     /**
@@ -43,7 +51,14 @@ public class Condition2 {
      * current thread must hold the associated lock.
      */
     public void wake() {
-	Lib.assertTrue(conditionLock.isHeldByCurrentThread());
+        Lib.assertTrue(conditionLock.isHeldByCurrentThread());
+        if(!waitQueue.isEmpty()){ // waitQueue가 비어있지 않다면 즉, thread가 들어있다면
+            boolean interrupt = Machine.interrupt().disable(); // 인터럽트를 disable시킴
+            KThread wakeThread = new KThread();
+            wakeThread = (KThread) waitQueue.removeFirst(); // waitQueue의 맨 앞 쓰레드를 꺼낸 다음,
+            wakeThread.ready(); // 꺼낸 쓰레드를 ready 상태로 만듦
+            Machine.interrupt().restore(interrupt); // disable된 인터럽트를 restore
+        }
     }
 
     /**
@@ -51,8 +66,66 @@ public class Condition2 {
      * thread must hold the associated lock.
      */
     public void wakeAll() {
-	Lib.assertTrue(conditionLock.isHeldByCurrentThread());
+        Lib.assertTrue(conditionLock.isHeldByCurrentThread());
+        while (!waitQueue.isEmpty()) {
+            wake();
+        }
     }
 
     private Lock conditionLock;
+    private LinkedList<KThread> waitQueue = new LinkedList<>();
+
+    // Place Condition2 testing code in the Condition2 class.
+
+    // Example of the "interlock" pattern where two threads strictly
+    // alternate their execution with each other using a condition
+    // variable.
+
+    private static class InterlockTest {
+        private static Lock lock;
+        private static Condition2 cv;
+
+        private static class Interlocker implements Runnable {
+            public void run () {
+                lock.acquire();
+                for (int i = 0; i < 10; i++) {
+                    System.out.println(KThread.currentThread().getName());
+                    cv.wake();   // signal
+                    cv.sleep();  // wait
+                }
+                lock.release();
+            }
+        }
+
+        public InterlockTest () {
+            lock = new Lock();
+            cv = new Condition2(lock);
+
+            KThread ping = new KThread(new Interlocker());
+            ping.setName("ping");
+            KThread pong = new KThread(new Interlocker());
+            pong.setName("pong");
+
+            ping.fork();
+            pong.fork();
+
+            // We need to wait for ping to finish, and the proper way
+            // to do so is to join on ping.  (Note that, when ping is
+            // done, pong is sleeping on the condition variable; if we
+            // were also to join on pong, we would block forever.)
+            // For this to work, join must be implemented.  If you
+            // have not implemented join yet, then comment out the
+            // call to join and instead uncomment the loop with
+            // yields; the loop has the same effect, but is a kludgy
+            // way to do it.
+            ping.join();
+            //for (int i = 0; i < 50; i++) { KThread.currentThread().yield(); }
+        }
+    }
+
+    // Invoke Condition2.selfTest() from ThreadedKernel.selfTest()
+
+    public static void selfTest() {
+        new InterlockTest();
+    }
 }
